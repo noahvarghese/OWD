116a1e84c4141c2abc182533ea88a49e3a9ebd40
noahvarghese <varghese.noah@gmail.com>
2020-06-28 15:18:42 -0400

    initial commit

---
+++ products.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Products</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="./assets/css/products.css"/>
+        <script type="text/javascript" src="./assets/js/products.js"></script>
+    </head>
+    <body>
+        <div id="main">
+            <?php include('./layout/nav.html') ?>
+            <h1>RESIDENTIAL SERVICES</h1>
+            <div>
+                <div id="windowInstallation">
+                    <div class="blurb">
+                        <p>
+                            If you’re looking for a window installation by a knowledgeable,
+                            experienced and family owned company, Oakville Windows &
+                            Doors can help. Our experienced team can help you choose the
+                            best type of window for your home’s look and your family’s needs.
+                        </p>
+                    </div>
+                    <div class="orange">
+                        <div>
+                            <h2>WINDOWS INSTALLATION</h2>
+                        </div>
+                    </div>
+                </div>
+                <div id="doorInstallation">
+                    <div class="gray">
+                        <div>
+                            <h2>DOOR INSTALLATION</h2>
+                        </div>
+                    </div>
+                    <div class="blurb">
+                        <p>
+                            Increase the curb appeal of your home with a new entry door
+                            system from Oakville Windows & Doors. Whether you need a
+                            replacement for your front door, back door, garden door, patio
+                            door or any other type of exterior door, we have a wide
+                            selection to fit your needs and style.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div id="windows">
+                <h3>Window Installation</h3>
+                <p>
+                    If you’re looking for a window installation by a knowledgeable, experienced and family owned
+                    company, Oakville Windows & Doors can help. Our experienced team can help you choose the best type of
+                    window for your home’s look and your family’s needs.
+                </p>
+                <div id=windowTypes>           
+                    <div id="vinylDiv">
+                        <img src="./assets/img/vinyl.png">
+                        <div id="vinylDesc">
+                            <div id="vinylHeader">
+                                <h4>01</h4>
+                                <h4>Vinyl Windows</h4>
+                            </div>
+                            <p>
+                                Vinyl windows are the most common types of windows we install, and
+                                they’re a popular choice for homeowners. Vinyl windows are an excellent
+                                investment, and will last years. They offer many benefits, including:
+                            </p>
+                            <ul id="vinylList" class="hyphen">
+                                <li>- Energy efficiency</li>
+                                <li>- Resistant to salt air and harsh industrial pollutants</li>
+                                <li>- Require virtually no maintenance</li>
+                                <li>- Wide variety of colours, styles and sizes</li>
+                                <li>- Affordable and long-lasting</li>
+                                <li>- Increase the value of your home</li>
+                                <li>- Lifetime guarantee</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div id="woodDiv">
+                        <img src="./assets/img/vinyl.png">
+                        <div id="woodDesc">
+                            <div id="woodHeader">
+                                <h4>02</h4>
+                                <h4>Wood Windows</h4>
+                            </div>
+                            <p>
+                                If you want a timeless and classic look for your home, wood
+                                windows are the option for you. They’re a beautiful choice
+                                for older homes and add lots of character, while still
+                                offering many of the same benefits as vinyl windows.
+                            </p>
+                            <ul id="woodList" class="hyphen">
+                                <li>- Keep your heritage home looking authentic</li>
+                                <li>- Can be painted or stained</li>
+                                <li>- Excellent insulation providing energy savings</li>
+                                <li>- Made from sustainable resources</li>
+                                <li>- High quality craftsmanship</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="orange seperator">
+                <p> We're proud to provide our customers with only the best brands of wood and vinyl windows.</p>
+            </div>
+            <div id="doors">
+                <h3>Door Installation</h3>
+                <p>
+                    Increase the curb appeal of your home with a new entry door system from Oakville Windows & Doors. Whether you need a
+                    replacement for your front door, back door, garden door, patio door or any other type of exterior door, we have a wide
+                    selection to fit your needs and style. Our doors feature a 3 lock system to keep your home safe.
+                    <br />
+                    From replacing a single door to upgrading all of the doors on your home for a beautiful, cohesive look, we work with you to
+                    find the door with the right style, fit and features for you and your home. 
+                </p>
+                <div id="doorTypes">
+                    <div id="fibreglassDiv">
+                        <img src="./assets/img/vinyl.png">
+                        <div id="fibreglassDesc">
+                            <div id="fibreglassHeader">
+                                <h4>01</h4>
+                                <h4>Fibreglass Doors</h4>
+                            </div>
+                            <p>
+                                Fibreglass doors are a high quality, durable alternative to wood and steel
+                                doors. They’re made to look like wood but have the benefit of being more
+                                economical and maintenance-free. We have a wide selection of colours
+                                and styles for you to choose from. But why choose fibreglass?
+                            </p>
+                            <ul id="fibreglassList" class="hyphen">
+                                <li>- Realistic Wood Appearance</li>
+                                <li>- Energy Efficient</li>
+                                <li>- Many Styles to Choose From</li>
+                                <li>- Customizable Configurations</li>
+                                <li>- Commercially Rated Door Frames</li>
+                                <li>- Multi-point Locking Systems for Added Strength & Security</li>
+                                <li>- Dent Resistant</li>
+                                <li>- Realistic Stain Finishes</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div id="steelDiv">
+                        <img src="./assets/img/vinyl.png">
+                        <div id="steelDesc">
+                            <div id="steelHeader">
+                                <h4>02</h4>
+                                <h4>Steel Doors</h4>
+                            </div>
+                            <p>
+                                You may not realize it, but steel doors are a remarkably
+                                versatile option for your home. They’re excellent at standing up to
+                                outdoor elements and are perfect for areas that experience lots of
+                                traffic, like your front door. Steel doors also have the
+                                following features:
+                            </p>
+                            <ul id="steelList" class="hyphen">
+                                <li>- Energy Efficient</li>
+                                <li>- Many Styles to Choose From</li>
+                                <li>- Customizable Configurations</li>
+                                <li>- Commercially Rated Door Frames</li>
+                                <li>- Multi-point Locking Systems for Added Strength & Security</li>
+                                <li>- Scratch Resistant</li>
+                                <li>- Thousands of colours to choose from</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="orange seperator">
+                    <p>
+                        Whether you’re looking for wood, fibreglass or steel
+                        door systems for your home, you can expect only
+                        the best quality from Oakville Windows & Doors.
+                        <br />
+                        <a href="#" alt="3Point" id="3Point">Learn more about our 3 lock system</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
