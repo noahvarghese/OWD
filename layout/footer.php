dbe56e2c693afdc28eabb964aab114a3db670742
noahvarghese <varghese.noah@gmail.com>
2020-07-05 18:47:04 -0400

    foote rfinished

--- layout/footer.php
+++ layout/footer.php
@@ -1,39 +1,41 @@
 <footer>
     <hr />
-    <div id="leftFooter">
-        <div id="location">
-            <div id="locationImg">
-                <img class="footerImg" alt="location" src="<?=Assets::imageAssets('location.png')?>"/>
+    <div id="footerContents">
+        <div id="leftFooter">
+            <div id="location">
+                <div id="locationImg">
+                    <img class="footerImg" alt="location" src="<?= Assets::imageAssets('location.png') ?>" />
+                </div>
+                <h3 class="footerHdr">OAKVILLE, ONTARIO</h3>
             </div>
-            <h3 class="footerHdr">OAKVILLE, ONTARIO</h3>
+            <h3 class="footerHdr">
+                Helping people find the
+                perfect window or door
+                for their home since 2010
+            </h3>
         </div>
-        <h3 class="footerHdr">
-            Helping people find the
-            perfect window or door
-            for their home since 2010
-        </h3>
-    </div>
-    <div id="rightFooter">
-        <span id="social">
-            <img class="social footerImg" id="instagram" alt="instagram" src="<?=Assets::imageAssets('instagramOrange.png')?>"/>
-            <img class="social footerImg" id="facebook" alt="facebook" src="<?=Assets::imageAssets('facebookOrange.png')?>"/>
-            <img class="social footerImg" id="twitter" alt="twitter" src="<?=Assets::imageAssets('twitterOrange.png')?>"/>
-            <img class="social footerImg" id="linkedIn" alt="linkedIn" src="<?=Assets::imageAssets('linkedInOrange.png')?>"/>
-            <img class="social footerImg" id="email" alt="email" src="<?=Assets::imageAssets('emailOrange.png')?>"/>
-        </span>
-        <div id="footerNav">
-            <div>
-                <h3 class="footerHdr">SERVICES</h3>
-                <h3 class="footerHdr">COMMERCIAL</h3>
-            </div> 
-            <div>
-                <h3 class="footerHdr">ABOUT</h3>
-                <h3 class="footerHdr">TESTIMONIALS</h3>
-            </div> 
-            <div>
-                <h3 class="footerHdr">BLOG</h3>
-                <h3 class="footerHdr">CONTACT</h3>
-            </div> 
+        <div id="rightFooter">
+            <span id="social">
+                <img class="social footerImg" id="instagram" alt="instagram" src="<?= Assets::imageAssets('instagramOrange.png') ?>" />
+                <img class="social footerImg" id="facebook" alt="facebook" src="<?= Assets::imageAssets('facebookOrange.png') ?>" />
+                <img class="social footerImg" id="twitter" alt="twitter" src="<?= Assets::imageAssets('twitterOrange.png') ?>" />
+                <img class="social footerImg" id="linkedIn" alt="linkedIn" src="<?= Assets::imageAssets('linkedInOrange.png') ?>" />
+                <img class="social footerImg" id="email" alt="email" src="<?= Assets::imageAssets('emailOrange.png') ?>" />
+            </span>
+            <div id="footerNav">
+                <div>
+                    <h3 class="footerHdr">SERVICES</h3>
+                    <h3 class="footerHdr">COMMERCIAL</h3>
+                </div>
+                <div>
+                    <h3 class="footerHdr">ABOUT</h3>
+                    <h3 class="footerHdr">TESTIMONIALS</h3>
+                </div>
+                <div>
+                    <h3 class="footerHdr">BLOG</h3>
+                    <h3 class="footerHdr">CONTACT</h3>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
