20b454976ca309e25bc2b0c4f33ad1a90f0509de
noahvarghese <varghese.noah@gmail.com>
2020-07-05 19:46:32 -0400

    nav doesn't shrink now

--- layout/nav.php
+++ layout/nav.php
@@ -2,9 +2,9 @@
     <img src="<?=Assets::imageAssets('logo.png')?>" alt="logo" id="logo"/>
     <ul id="nav">
         <li><a href="#">HOME</a></li>
-        <li><a href="#">SERVICES</a></li>
+        <li><a href="../pages/services.php">SERVICES</a></li>
         <li><a href="#">GALLERY</a></li>
-        <li><a href="#">TESTIMONIALS</a></li>
+        <li><a href="../pages/testimonials.php">TESTIMONIALS</a></li>
         <li><a href="#">CONTACT</a></li>
         <li><a href="#">BLOGS</a></li>
         <li><a href="#">COMMERCIAL</a></li>
--- pages/testimonials.php
+++ pages/testimonials.php
@@ -1,35 +1,41 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Testimonials</title>
-        <meta charset="utf-8"/>
-        <link rel="stylesheet" href="./assets/css/testimonials.css"/>
-        <script type="text/javascript" src="./assets/js/testimonials.js"></script>
-    </head>
-    <body>
-        <div id="main">
-            <?php include('../layout/nav.html') ?>
-            <h1>TESTIMONIALS</h1>
-            <h2 id="blurb">
-                Find out first-hand why our customers love<br />
-                working with Oakville Windows & Doors!
-            </h2>
-            <div id="testimonials">
-                <div class="individual">
-                    <div class="quote">
-                        <img />
-                    </div>
-                    <div class="from">
-                        <h3></h3>
-                        <h3></h3>
-                        <hr />
-                    </div>
-                    <div class="text">
-                        <h4></h4>
-                        <p></p>
-                    </div>
+
+<head>
+    <title>Testimonials</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="<?= Assets::styleAssets('testimonials.css') ?>" />
+    <link rel="stylesheet" href="<?= Assets::styleAssets('nav.css') ?>" />
+    <link rel="stylesheet" href="<?= Assets::styleAssets('footer.css') ?>" />
+    <script type="text/javascript" src="<?= Assets::jsAssets('services.js') ?>"></script>
+</head>
+
+<body>
+    <div id="main">
+        <?php include(Assets::nav()) ?>
+        <h1>TESTIMONIALS</h1>
+        <h2 id="blurb">
+            Find out first-hand why our customers love<br />
+            working with Oakville Windows & Doors!
+        </h2>
+        <div id="testimonials">
+            <div class="individual">
+                <div class="quote">
+                    <img />
+                </div>
+                <div class="from">
+                    <h3></h3>
+                    <h3></h3>
+                    <hr />
+                </div>
+                <div class="text">
+                    <h4></h4>
+                    <p></p>
                 </div>
             </div>
         </div>
-    </body>
+        <?php include(Assets::footer()) ?>
+    </div>
+</body>
+
 </html>
