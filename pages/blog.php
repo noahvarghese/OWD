f7f229ce06d20d3a1d3076cdf8c858fdad104de3
noahvarghese <varghese.noah@gmail.com>
2020-07-25 17:40:20 -0400

    main blog html added

--- pages/blog.php
+++ pages/blog.php
@@ -20,8 +20,61 @@
         </div>
     </div>
     <div id="older">
-        <div>
-
+        <div class="horizontal">
+            <div>
+                <img src="<?= IMG_PATH . "tempBlog2.jpg" ?>"/>
+                <div>
+                    <h2>How Your Old Windows & Doors May Be Affecting Your Home?</h2>
+                    <p class="blogDescription">
+                        Every homeowner knows that windows and doors are an
+                        important part of their house, but do you know how your old
+                        windows and doors might be impacting your home? 1. Curb
+                        appeal Curb appeal is....
+                        <a href="#" class="readMore">READ MORE</a>
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="<?= IMG_PATH . "tempBlog3.jpg" ?>"/>
+                <div>
+                    <h2>Window Installations: Retrofit Vs. Brick-To-Brick</h2>
+                    <p class="blogDescription">
+                        At Oakville Windows & Doors, it’s important to us that you
+                        understand the window installation process so you can make
+                        informed decisions. Some homeowners may not realize that
+                        there are two...
+                        <a href="#" class="readMore">READ MORE</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="horizontal">
+            <div>
+                <img src="<?= IMG_PATH . "tempBlog4.jpg" ?>"/>
+                <div>
+                    <h2>How To Increase Your Home’s Natural Light With Windows</h2>
+                    <p class="blogDescription">
+                        While electricity and lighting can do wonders in your home,
+                        nothing beats having natural light. While there are certain tricks
+                        you can use in your home to bring more light into your space,
+                        like...
+                        <a href="#" class="readMore">READ MORE</a>
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="<?= IMG_PATH . "tempBlog5.jpg" ?>"/>
+                <div>
+                    <h2>How To Choose A New Front Door</h2>
+                    <p class="blogDescription">
+                        Your front door is a major part of your home, both aesthetically
+                        and functionally, so it’s important to choose the right one. If
+                        you are looking to replace your front door, here’s what you
+                        should... 
+                        <a href="#" class="readMore">READ MORE</a>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
