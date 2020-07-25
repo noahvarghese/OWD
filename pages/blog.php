3ce623662d86b494df037160bec5f1a556e0ad1f
noahvarghese <varghese.noah@gmail.com>
2020-07-25 17:34:37 -0400

    main blog page

---
+++ pages/blog.php
@@ -0,0 +1,27 @@
+<h1>OUR BLOG</h1>
+<p id="pageDescription">
+    At Oakville Windows & Doors, we’re committed to providing you with excellent
+    service – even after your window or door installation is finished. Stay up to date
+    with all of the latest industry trends, maintenance advice and need-to-know
+    information for your new windows and doors with our blog.
+</p>
+<div id="container">
+    <div id="newest">
+        <img id="banner" alt="bannerImage" src="<?= IMG_PATH . "tempBlog1.jpg" ?>"/>
+        <div>
+            <h2>DOES HUMIDITY AFFECT WINDOWS?</h2>
+            <p class="blogDescription" id="bannerDescription">
+                At Oakville Windows & Doors, we’re committed to providing you with excellent
+                service – even after your window or door installation is finished. Stay up to date
+                with all of the latest industry trends, maintenance advice and need-to-know
+                information for your new windows and doors with our blog.
+                <a href="#" class="readMore">READ MORE</a>
+            </p>
+        </div>
+    </div>
+    <div id="older">
+        <div>
+
+        </div>
+    </div>
+</div>
