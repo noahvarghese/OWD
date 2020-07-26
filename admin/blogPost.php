4b5690c1c1f29b2a700b13933e56c2d95cec8f43
noahvarghese <varghese.noah@gmail.com>
2020-07-25 22:50:38 -0400

    added admin blog post page

---
+++ admin/blogPost.php
@@ -0,0 +1,24 @@
+<form>
+    <div>
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title"/>
+    </div>
+    <div>
+        <label for="intro">Introduction:</label>
+        <textarea id="intro" name="intro"></textarea>
+    </div>
+    <h3>Section 1</h3>
+    <div>
+        <label for="heading1">Section 1 Header:</label>
+        <input type="text" id="heading1" name="heading1" class="header">
+    </div>
+    <div> 
+        <label for="content1">Section 1 Content:</label>
+        <textarea name="content1" id="content1" class="content"></textarea>
+    </div>
+    <button value="Add Section"></button>
+    <div>
+        <input type="submit" value="Save"/>
+        <input type="submit" value="Publish"/>
+    </div>
+</form>
