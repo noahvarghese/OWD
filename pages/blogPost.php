d7532852bb172af98e3dd09553f3fb7701ae66a1
noahvarghese <varghese.noah@gmail.com>
2020-07-25 20:38:14 -0400

    added container for images

--- pages/blogPost.php
+++ pages/blogPost.php
@@ -1,4 +1,4 @@
-<img class="banner" alt="blogBanner" src="<?= IMG_PATH . "tempBlog1.jpg" ?>"/>
+<img class="banner" alt="blogBanner" src="<?= IMG_PATH . "tempBlog1.jpg" ?>" />
 <h1>DOES HUMIDITY AFFECT WINDOWS?</h1>
 <div id="description">
     <p>
@@ -75,19 +75,27 @@
     <h3>RECENT POSTS</h3>
     <div>
         <div class="recentBlog">
-            <img alt="small1" src="<?= IMG_PATH . "tempBlogSmall1.jpg" ?>"/>
+            <div class="imgContainer">
+                <img alt="small1" src="<?= IMG_PATH . "tempBlogSmall1.jpg" ?>" />
+            </div>
             <h4>How Your old Windows & Doors May Be Affecting Your Home?</h4>
         </div>
         <div class="recentBlog">
-            <img alt="small2" src="<?= IMG_PATH . "tempBlogSmall2.jpg" ?>"/>
+            <div class="imgContainer">
+                <img alt="small2" src="<?= IMG_PATH . "tempBlogSmall2.jpg" ?>" />
+            </div>
             <h4>Window Installations: Retrofit Vs. Brick-To-Brick</h4>
         </div>
         <div class="recentBlog">
-            <img alt="small3" src="<?= IMG_PATH . "tempBlogSmall3.jpg" ?>"/>
+            <div class="imgContainer">
+                <img alt="small3" src="<?= IMG_PATH . "tempBlogSmall3.jpg" ?>" />
+            </div>
             <h4>How To Increase Your Home’s Natural Light With Windows</h4>
         </div>
         <div class="recentBlog">
-            <img alt="small4" src="<?= IMG_PATH . "tempBlogSmall4.jpg" ?>"/>
+            <div class="imgContainer">
+                <img alt="small4" src="<?= IMG_PATH . "tempBlogSmall4.jpg" ?>" />
+            </div>
             <h4>How To Choose A New Front Door</h4>
         </div>
     </div>
