538c4e3091ca6eaec0c9cfd5ca8a1938a157544b
noahvarghese <varghese.noah@gmail.com>
2020-08-12 19:28:55 -0400

    added check if docker is installed, added check if docker is running, and then start and enable it

--- setup/install.sh
+++ setup/install.sh
@@ -2,10 +2,39 @@
 
 # script to setup docker and build docker file
 
+DIR=`realpath $0`
+
+
 if [ "$EUID" -ne 0 ]
     then echo "Please run as root"
     exit
 fi
 
-sudo systemctl start docker
-sudo systemctl enable docker
+if ! command -v docker &> /dev/null
+    then echo "Please install docker and retry the script."
+    exit
+fi
+
+pgrep docker >/dev/null 2>&1
+
+#check return status of last command
+if [ $? -eq 0 ]
+
+    sudo systemctl start docker >/dev/null 2>&1
+
+    if [ $? -ne 0 ]
+        then echo "Unable to start docker, please check 'systemctl status docker' OR 'journalctl -xe'"
+        exit
+    fi
+
+    sudo systemctl enable docker >/dev/null 2>&1
+
+    if [ $? -ne 0 ]
+        then echo "Unable to enable docker service, please enable manually."
+    fi
+fi
+
+docker build $DIR/dockerfile
+
+
+
