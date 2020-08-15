bf13fe9811db5ee552d4f4e8910e8b3b3b49f604
noahvarghese <varghese.noah@gmail.com>
2020-08-15 12:34:11 -0400

    got install script working, got initial docker file working, just need to add copy of files

--- setup/install.sh
+++ setup/install.sh
@@ -1,21 +1,31 @@
 #! /bin/bash
 
+function printHelp {
+    echo "Mandatory arguments:"
+    echo "    -n | --name {NAME}    container name"
+    echo "Optional arguments"
+    echo "    -bg | --background    if option is set, specifies if container will run detached or not"
+}
+
 # script to setup docker and build docker file
 
-DIR=`realpath $0`
+# gets full path including script
+#SOURCE=`realpath $0`
 
-if [ "$EUID" -ne 0 ]
-    then echo "Please run as root"
-    exit
+# gets directory path to script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+# displays all arguments
+if test "$#" -eq 0; then
+    printHelp
+    exit 1
 fi
 
-if ! command -v docker &> /dev/null
-    then echo "Please install docker and retry the script."
-    exit
-fi
+NAMEFOUND=false
 
 # check for docker name and whether to run in the background or not
-while test $# -gt 0; do
+while test "$#" -gt 0; do
     case "$1" in
         -bg|--background)
             shift
@@ -24,38 +34,70 @@ while test $# -gt 0; do
         -n|--name)
             shift
             if test $# -gt 0; then
-                export NAME=$1
+                if test $1 == "-h"; then
+                    printHelp
+                    exit 1
+                elif test $1 == "--help"; then
+                    printHelp
+                    exit 1
+                else
+                    export NAME=$1
+                    NAMEFOUND="true"
+                fi
             else
                 echo "No image name specified"
                 exit 1
             fi
             shift
             ;;
+        -h|--help)
+            shift
+            printHelp
+            exit 1
+            ;;
     esac
 done
+
+if [ $NAMEFOUND != "true" ]; then
+    echo "Name is required!"
+    printHelp
+    exit 1
+fi
+
+#check if running as root
+if test "$EUID" -ne 0; then
+    echo "Please run as root"
+    exit 1
+fi
+
+# Check if docker installed
+if ! command -v docker &> /dev/null; then
+    echo "Please install docker and retry the script."
+    exit 1
+fi
 
 # check if docker is running
 pgrep docker >/dev/null 2>&1
 
 #check return status of last command
-if [ $? -eq 0 ]
-
+if [ $? -ne 0 ]; then
+    echo "Starting docker..."
     sudo systemctl start docker >/dev/null 2>&1
 
-    if [ $? -ne 0 ]
-        then echo "Unable to start docker, please check 'systemctl status docker' OR 'journalctl -xe'"
-        exit
+    if test "$?" -ne 0; then
+        echo "Unable to start docker, please check 'systemctl status docker' OR 'journalctl -xe'"
+        exit 1
     fi
 
+    echo "Enabling docker..."
     sudo systemctl enable docker >/dev/null 2>&1
 
-    if [ $? -ne 0 ]
-        then echo "Unable to enable docker service, please enable manually."
+    if test "$?" -ne 0; then
+        echo "Unable to enable docker service, please enable manually."
+        exit 1
     fi
 fi
-
+echo "Building container..."
 docker build $DIR/. --tag $NAME
-docker run $NAME
-
-
-
+echo "Running container..."
+#docker run $NAME --name $NAME -d 80:80 443:443
