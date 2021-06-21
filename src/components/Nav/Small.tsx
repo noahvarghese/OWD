9f5d967eded263c8469006e74f109668508739ba
noahvarghese <varghese.noah@gmail.com>
2021-06-21 23:49:48 +0000

    styled header

--- src/components/Nav/Small.tsx
+++ src/components/Nav/Small.tsx
@@ -1,37 +1,55 @@
 import React from "react";
 import { pages } from "./pages";
 import { Link } from "react-router-dom";
+import Logo from "../../assets/img/logo.png";
+import "./Small.scss";
 
 const SmallNav: React.FC = () => {
     return (
-        <ul className="SmallNav">
-            {pages.map((page, index) => (
-                <li key={index}>
-                    <Link to={page.path}>{page.title}</Link>
-                    {page.subMenu ? (
-                        <div className="SmallSubMenuContainer">
-                            {" "}
-                            {page.subMenu.map((sub) => {
-                                return (
-                                    <>
-                                        <h4>{sub.title}</h4>
-                                        <ul>
-                                            {sub.list.map((item, listIndex) => (
-                                                <li key={listIndex}>
-                                                    <Link to={item.path}>
-                                                        {item.title}
-                                                    </Link>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </>
-                                );
-                            })}
-                        </div>
-                    ) : null}
-                </li>
-            ))}
-        </ul>
+        <div id="SmallNav">
+            <div id="SmallNavContainer">
+                <button id="menuToggle">
+                    <div className="first"></div>
+                    <div className="second"></div>
+                    <div className="third"></div>
+                </button>
+                <Link to="/" className="logoContainer">
+                    <img src={Logo} alt="Logo" />
+                </Link>
+            </div>
+            <ul>
+                {pages.map((page, index) => (
+                    <li key={index}>
+                        <Link to={page.path}>{page.title}</Link>
+                        {page.subMenu ? (
+                            <div className="SmallSubMenuContainer">
+                                {" "}
+                                {page.subMenu.map((sub) => {
+                                    return (
+                                        <>
+                                            <h4>{sub.title}</h4>
+                                            <ul>
+                                                {sub.list.map(
+                                                    (item, listIndex) => (
+                                                        <li key={listIndex}>
+                                                            <Link
+                                                                to={item.path}
+                                                            >
+                                                                {item.title}
+                                                            </Link>
+                                                        </li>
+                                                    )
+                                                )}
+                                            </ul>
+                                        </>
+                                    );
+                                })}
+                            </div>
+                        ) : null}
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
