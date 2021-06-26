2c42a81e71c1a354e72c4e98a2d45315f1393872
noahvarghese <varghese.noah@gmail.com>
2021-06-26 21:34:11 +0000

    more responsiveness and animations

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -8,6 +8,7 @@ import "./Nav.scss";
 import { connect } from "react-redux";
 import { State } from "../../types/state";
 import { Link } from "react-router-dom";
+import { isBrowser } from "react-device-detect";
 
 export interface NavProps {
     paths: { title: string; path: string }[];
@@ -22,21 +23,23 @@ const Nav: React.FC<NavProps> = ({ paths }) => {
             <MediaQuery minWidth={1366}>
                 <LargeNav />
             </MediaQuery>
-            <div id="subNav">
-                <ul id="breadcrumbs">
-                    <li>
-                        <Link to="/" id="Home">
-                            <img src={Home} alt="Home" loading="lazy" />
-                        </Link>
-                    </li>
-                    {paths.map((path) => (
-                        <li key={path.title} className="path">
-                            <Link to={path.path}>{path.title}</Link>
+            {isBrowser && (
+                <div id="subNav">
+                    <ul id="breadcrumbs">
+                        <li>
+                            <Link to="/" id="Home">
+                                <img src={Home} alt="Home" loading="lazy" />
+                            </Link>
                         </li>
-                    ))}
-                </ul>
-                <PrimaryButton text="Request a quote" link="/contact" />
-            </div>
+                        {paths.map((path) => (
+                            <li key={path.title} className="path">
+                                <Link to={path.path}>{path.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <PrimaryButton text="Request a quote" link="/contact" />
+                </div>
+            )}
         </nav>
     );
 };
