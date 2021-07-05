0f4949d745d5768b383476ec5213861a1c793b91
Noah Varghese <varghese.noah@gmail.com>
2021-07-05 18:58:39 -0400

    got the review blurbs displaying correctly

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState } from "react";
 import Quote from "../assets/img/quotation1.png";
 // import Star from "../assets/img/star.png";
 import "./Review.scss";
@@ -6,43 +6,14 @@ import { ReviewAttributes } from "../data/reviews";
 import DropdownArrow from "./Arrows/DropdownArrow";
 
 const Review: React.FC<ReviewAttributes> = (props) => {
-    const [contentRef, setContentRef] = useState<HTMLDivElement | null>(null);
-    const [pRef, setPRef] = useState<HTMLParagraphElement | null>(null);
     const [display, setDisplay] = useState<boolean>(
         props.review.length <= 140 || false
     );
 
-    const changeHeight = useCallback(
-        (_?: any) => {
-            if (contentRef !== null && pRef !== null) {
-                if (!display) {
-                    contentRef.style.height = `${
-                        pRef.clientHeight +
-                        (props.review.length > 140 ? 7 : 4) * 16
-                    }px`;
-                } else {
-                    contentRef.style.height = "max-content";
-                }
-            }
-        },
-        [contentRef, pRef, props.review.length, display]
-    );
-
-    useEffect(() => {
-        if (contentRef !== null && pRef !== null) {
-            changeHeight();
-            try {
-                window.removeEventListener("resize", changeHeight);
-            } catch (_) {}
-
-            window.addEventListener("resize", changeHeight);
-        }
-    }, [contentRef, pRef, changeHeight]);
-
     return (
         <div className="Review">
             <h2>{props.name}</h2>
-            <div className="contents" ref={setContentRef}>
+            <div className={(display ? "show " : "") + "contents"}>
                 <div className="imgContainer">
                     <img src={Quote} alt="quote" />
                 </div>
@@ -50,12 +21,8 @@ const Review: React.FC<ReviewAttributes> = (props) => {
                     <img src={Star} alt="star" />
                     <span>{props.rating}</span>
                 </div> */}
-                <p ref={setPRef} className={display ? "show" : ""}>
-                    {props.review.length > 140
-                        ? display
-                            ? props.review
-                            : props.review.substr(0, 140)
-                        : props.review}
+                <p className={display ? "show" : ""}>
+                    {props.review}
                 </p>
                 {props.review.length > 140 && (
                     <div className="tail">
